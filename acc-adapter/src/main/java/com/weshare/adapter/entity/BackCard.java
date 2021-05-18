d8671878d692d99b6abf7f3dc03cd1597635163e
scy <348691356@qq.com>
2021-05-18 14:50:07 +0800

    0000

---
+++ acc-adapter/src/main/java/com/weshare/adapter/entity/BackCard.java
@@ -0,0 +1,39 @@
+package com.weshare.adapter.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.weshare.service.api.entity.UserBaseReq;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+
+/**
+ * @author: scyang
+ * @program: acc
+ * @package: com.weshare.adapter.entity
+ * @date: 2021-05-18 14:39:09
+ * @describe:
+ */
+@Data
+@Accessors(chain = true)
+@Entity
+//@EntityListeners(AuditingEntityListener.class)
+@Table(indexes = {@Index(name = "back_card_due_bill_no",unique = true,columnList = "dueBillNo")})
+@org.hibernate.annotations.Table(appliesTo = "back_card",comment = "银行卡号信息")
+public class BackCard {
+
+    @Id
+    private String id;
+    @Column(columnDefinition = "varchar(50) not null comment '用户id'")
+    private String userId;
+    @Column(columnDefinition = "varchar(50) not null comment '借据号'")
+    private String dueBillNo;
+    @Column(columnDefinition = "varchar(50) not null comment '用户名'")
+    private String userName;
+    @Column(columnDefinition = "varchar(50) not null comment '银行名'")
+    private UserBaseReq.BackName backName;
+    @Column(columnDefinition = "varchar(50) not null comment '银行代码'")
+    private String backCode;
+    @Column(columnDefinition = "varchar(50) not null comment '银行卡号'")
+    private String backNum;
+}
---
+++ acc-adapter/src/main/java/com/weshare/adapter/entity/LinkMan.java
@@ -0,0 +1,49 @@
+package com.weshare.adapter.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.weshare.service.api.entity.UserBaseReq;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+
+/**
+ * @author: scyang
+ * @program: acc
+ * @package: com.weshare.adapter.entity
+ * @date: 2021-05-18 14:27:04
+ * @describe:
+ */
+@Data
+@Accessors(chain = true)
+@Entity
+//@EntityListeners(AuditingEntityListener.class)
+@Table(indexes = {@Index(name = "link_man_due_bill_no_index", unique = true, columnList = "dueBillNo")})
+@org.hibernate.annotations.Table(appliesTo = "link_man", comment = "联系人信息表")
+public class LinkMan {
+
+    @Id
+    private String id;
+    @Column(columnDefinition = "varchar(50) not null comment '用户id'")
+    private String userId;
+    @Column(columnDefinition = "varchar(50) not null comment '借据号'")
+    private String dueBillNo;
+    @Column(columnDefinition = "varchar(50) not null comment '用户名'")
+    private String userName;
+    @Column(columnDefinition = "varchar(50) not null comment '性别'")
+    private String sex;
+    @Column(columnDefinition = "varchar(50) not null comment '手机号码'")
+    private String iphone;
+    @Column(columnDefinition = "varchar(50) not null comment '身份证类型'")
+    @Enumerated(EnumType.STRING)
+    private UserBaseReq.IdCardType idCardType;
+    @Column(columnDefinition = "varchar(50) not null comment '省份证号码'")
+    private String idCardNum;
+    @Column(columnDefinition = "varchar(50) not null comment '工作类型'")
+    private String workType;
+    @Column(columnDefinition = "varchar(50) not null comment '地址'")
+    private String address;
+    @Column(columnDefinition = "varchar(50) not null comment '关系类型'")
+    @Enumerated(EnumType.STRING)
+    private UserBaseReq.RelationalType relationalType;
+}
