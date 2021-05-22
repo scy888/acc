ed24123fdb4549141a18ba868785bb527738ca68
scy <348691356@qq.com>
2021-05-22 14:14:05 +0800

    0000

--- acc-adapter/src/main/java/com/weshare/adapter/controller/AdapterController.java
+++ acc-adapter/src/main/java/com/weshare/adapter/controller/AdapterController.java
@@ -12,17 +12,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.Serializable;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
