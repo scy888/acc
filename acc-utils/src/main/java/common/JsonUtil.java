d93e22710a50393d6bfb557ccbaf23ceee8d1306
scy <348691356@qq.com>
2021-07-13 18:36:39 +0800

    json工具类序列化

--- acc-utils/src/main/java/common/JsonUtil.java
+++ acc-utils/src/main/java/common/JsonUtil.java
@@ -21,7 +21,11 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * JSON工具类(使用Jackson做为实现)
@@ -145,4 +149,45 @@ public class JsonUtil {
         return null;
     }
 
+
+    /**
+     * 把Map对象里，含有子json串的字符串(String)，还原成Map对象
+     *
+     * @param map
+     * @return
+     */
+    public static Map<String, Object> format(Map<String, Object> map) {
+        map.forEach((k, v) -> {
+            if (v != null && v.toString().startsWith("{\"") && v.toString().endsWith("}")) {
+                map.put(k, format(fromJson(v.toString(), Map.class)));
+            } else if (v != null && v.toString().startsWith("[{\"") && v.toString().contains("]")) {
+                List<Map<String, Object>> list = fromJson(v.toString(), List.class);
+                List<Map<String, Object>> newlist = new ArrayList<>();
+                for (Map<String, Object> innerMap : list) {
+                    newlist.add(format(innerMap));
+                }
+                map.put(k, newlist);
+            } else if (v != null && v.toString().startsWith("{")) {
+                map.put(k, format((Map<String, Object>) v));
+            } else if (v != null && v.toString().startsWith("[")) {
+                List<Map<String, Object>> list = (List<Map<String, Object>>) v;
+                List<Map<String, Object>> newlist = new ArrayList<>();
+                for (Map<String, Object> innerMap : list) {
+                    newlist.add(format(innerMap));
+                }
+                map.put(k, newlist);
+            }
+        });
+        return map;
+    }
+    /**
+     * 把非标的JSON串（含有大量反斜杠等转义），转为标准JSON字符串，用法，参考：JsonUtilTest#revertToStandardJsonString等3个例子。
+     *
+     * @param nonStandardJsonString 含有转义符的非标JSON
+     * @param prettyPrint           是否格式化
+     * @return 标准JSON字符串
+     */
+    public static String revertToStandardJsonString(String nonStandardJsonString, boolean prettyPrint) {
+        return toJson(format(fromJson(nonStandardJsonString, Map.class)), prettyPrint);
+    }
 }
